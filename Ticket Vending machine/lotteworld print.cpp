ffa31fc01474f597b7ac8bd8aa15228f642016d1
SWYeo118 <effortmeta@gmail.com>
2022-03-24 10:01:11 +0900

    data 출력 저장

--- Ticket Vending machine/lotteworld print.cpp
+++ Ticket Vending machine/lotteworld print.cpp
@@ -9,9 +9,8 @@ int main()
 	struct tm tm = *localtime(&t);
 	
 	int proceed;
-	int index = 0, sumPay = 0, reOrder=0;
-	int ticketType[10] = {0,}, ticketNum[10] = {0,}, advantage[10] = {0,}, totalPay[10] = {0,}, citizenNum[10] = {0,}, year[10] = {0,}, numBack[10] = {0,}, calYear[10] = {0,}, norPay[10] = {0,};
-	int month[10] = {0,}, day[10] = {0,}, gap[10] = {0,};
+	int reOrder=0;
+
 	const int MorNor = 59000;
 	const int MorTeen = 52000;
 	const int MorKid = 47000;
@@ -27,7 +26,6 @@ int main()
 	do
 	{
 
-	
 	int index = 0, sumPay = 0;
 	int ticketType[10] = {0,}, ticketNum[10] = {0,}, advantage[10] = {0,}, totalPay[10] = {0,}, citizenNum[10] = {0,}, year[10] = {0,}, numBack[10] = {0,}, calYear[10] = {0,}, norPay[10] = {0,};
 	int month[10] = {0,}, day[10] = {0,}, gap[10] = {0,};
@@ -35,7 +33,6 @@ int main()
 	
 	do
 	{ 
-	FILE *fp = fopen("reprot.csv", "a+");
 	
 	printf("%18s %d년 %d월 %d일 %02d:%02d:%02d\n\n", "현재시각 :",tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec);
 	printf("v 놀이공원 티켓 발권 서비스에 오신 것을 환영합니다.\n\n");
@@ -126,7 +123,7 @@ int main()
 		{for(;;)
 			{
 				printf("우대사항을 잘못 선택하셨습니다. 다시 선택하세요\n1. 없음\n2. 장애인\n3. 국가유공자\n4. 휴가장병\n5. 임산부\n6. 다둥이 행복카드\n");
-	scanf("%d", &advantage[index]); 
+				scanf("%d", &advantage[index]); 
 		if ((advantage[index] == 1 || advantage[index] == 2 || advantage[index] == 3 || advantage[index] == 4 || advantage[index] == 5 || advantage[index] == 6 ))
 			break;
 		}
@@ -158,18 +155,17 @@ int main()
 	totalPay[index] = (ticketNum[index] * norPay[index]);
 	printf("총 티켓 가격은 %d 원 입니다.\n계속해서 발권을 진행하시겠습니까? 1.예/2.아니오\n", totalPay[index]);
 	scanf("%1d", &proceed);  //여기까지 index 값 = 0
-	
-	fprintf(fp, "%d년,%d월,%d일",nowYear, nowMonth, nowDays);
-	 // 받아야 하는 정보 : 날짜,  권종, 연령, 수량, 우대사항 
-	fclose(fp);
-
 	index++;	//이 아래부터 index 값 = 1
 	}
 	while(proceed<=1);
 	printf("이용해주셔서 감사합니다.\n\n");
 	printf("===============결제내역==============\n\n");
+	
+	FILE *fp = fopen("report.csv", "a+");
+
 	for(int i = 0; i<index;i++) 
 	{
+
 		if ((ticketType[i]) == 1 )
 		{
 			printf("주간권 ");
@@ -219,15 +215,17 @@ int main()
 		else if(advantage[i]==6)
 		 {
 		printf("다둥이 행복카드 우대적용\n");} 
-
 		sumPay += totalPay[i];
+		
+	fprintf(fp, "%d,%d,%d,%d,%d,%d,%d,%d\n", tm.tm_year+1900, tm.tm_mon+1, tm.tm_mday, ticketType[i], calYear[i], ticketNum[i], advantage[i], totalPay[i]);
+	
 	}
-	 
 	printf("총 결제 금액 : %d 원\n\n", sumPay);
 	printf("=====================================\n");
 	printf("\n- 새롭게 발권을 진행하시겠습니까? 1.예 / 2.아니오\n");
 	scanf("%1d", &reOrder);
 	
+ 	fclose(fp);
 	}while(reOrder<2);
 	return 0;
 }
